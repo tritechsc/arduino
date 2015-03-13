071e819f1623ab39b36f57e9b26ba08de51ab270
TriTech <craig.coleman@ksd.org>
2015-03-13 09:45:43 -0700

    Create simple-serial-test.ino

---
+++ simple-serial-test.ino
@@ -0,0 +1,22 @@
+int incomingByte = 0;   // for incoming serial data
+int i = 0;
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+       
+
+        Serial.println();
+        i++;
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+        }
+}
