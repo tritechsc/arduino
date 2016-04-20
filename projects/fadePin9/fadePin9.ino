323d5eb76d5d2f4098ba003e893ee26f36efb7bc
Tri-Tech <craig.coleman@ksd.org>
2016-04-20 14:59:12 -0700

    Update fadePin9.ino

--- projects/fadePin9/fadePin9.ino
+++ projects/fadePin9/fadePin9.ino
@@ -1,4 +1,4 @@
-int led = 6;           // the pin that the LED is attached to
+int led = 9;           // the pin that the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
