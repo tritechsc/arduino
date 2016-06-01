50b7f3dd3b5d75ac55fc7c2d4f5555c13c3b72b3
Craig Coleman <craigcoleman@users.noreply.github.com>
2016-06-01 05:42:08 -0700

    Update RGBW-circular.ino

--- RGBW-circular.ino
+++ RGBW-circular.ino
@@ -5,17 +5,12 @@
 #endif
 
 #define PIN 6
-
 #define NUM_LEDS 16
-
 #define BRIGHTNESS 25
-
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
-
 void setup() {
-  Serial.begin(115200);
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  Serial.begin(9600);
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
@@ -27,32 +22,44 @@ void setup() {
 }
 
 //set up variables
-int w = 0;
-int i; 
 int red,green,blue,white;
-int first,last;
+//set first led and total leds
+int first = 10;
+int leds = 16;
+int count = 0;
 
+//The event loop  *************************
 void loop() {
-  red = 127; green = 60; blue = 0; white = 0;
-  circular(first,last,red,green,blue,white);
+  if (count == 0){
+    //this sets up the leds for the first time through the loop
+      red = 0; green = 0; blue = 255; white = 0;
+  }
+  
+    red = 255;
+    if(count % 2 == 0) {red = 0;}
+ 
+  circular(first,leds,red,green,blue,white);
+  //if (count ==0) delay(3000);
+  first = first + 1;
+  if (first > (leds -1)) first = 0;//reset first to 0 if it is greater than the number of leds
+  count++;
+  if(count > 1000) count = 1; //never set count to 0
 }
 
-
+// end of the event loop *************************
 
 //red function
-void circular(int s, int e,int r,int g,int b,int w){
-
-  if (s = 0)
-    {e = 15;}
-   else{e = s - 1;}
-   
-int delayVal = 100;
+void circular(int s, int lds, int  r,int g,int b,int w){
+// s:start   lds:the number of leds  r:red  g:green  b:blue  w:white
+int delayVal = 100;// change the delayVal to speed things up
 int j;
+Serial.println();//debug
 for(j = 0; j < 16 ; j++){
-          strip.setPixelColor(j, strip.Color(r,g,b,w ));
-          r = r+ 3;
+    Serial.println (s);//debug
+          strip.setPixelColor(s, strip.Color(r,g,b,w ));
           delay(delayVal);
            strip.show();  
+            s++; // increase s by 1;
+            if (s > (lds -1)) { s = 0;}  
  }
-
 }
