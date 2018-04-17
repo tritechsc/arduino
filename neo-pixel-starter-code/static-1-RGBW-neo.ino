5d16face1f750a5f631f10ddca412371cc5b21aa
Tri-Tech <craig.coleman@ksd.org>
2018-04-17 14:58:46 -0700

    Add files via upload

---
+++ neo-pixel-starter-code/static-1-RGBW-neo.ino
@@ -0,0 +1,52 @@
+// RGBW (Red Green Blue White Neo-Pixel starter code
+// 16 LEDS with no loop inside of void loop()
+// CW Coleman 180417
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+#define NUM_LEDS 16
+
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+// Initialize some variables for the void loop()
+int led, red, green, blue, white;
+int wait = 100;
+
+void loop() {
+// turn on led 0 - - -
+  led = 0; 
+  red = 255; 
+  green = 0; 
+  blue = 0; 
+  white = 10; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+  
+// turn on the next led
+  led = led +1 ; red = 0; green = 255; blue = 0; white = 10; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+
+  
+   
+}
+
+
+
