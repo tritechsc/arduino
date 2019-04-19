f44cc8f7b4d623673de66d07a1c37bc0ec4bc6d1
Tri-Tech <craig.coleman@ksd.org>
2019-04-18 18:42:18 -0700

    Add files via upload

---
+++ ultrasonic/UltraSonic_78.ino
@@ -0,0 +1,51 @@
+/*
+ HC-SR04 Ping distance sensor]
+ VCC to arduino 5v GND to arduino GND
+ Echo to Arduino pin 13 Trig to Arduino pin 12
+ Red POS to Arduino pin 11
+ Green POS to Arduino pin 10
+ 560 ohm resistor to both LED NEG and GRD power rail
+ More info at: http://goo.gl/kJ8Gl
+ Original code improvements to the Ping sketch sourced from Trollmaker.com
+ Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+ */
+// edited by cwc
+
+#define trigPin 7
+#define echoPin 8
+#define led13 13
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led13, OUTPUT);
+
+}
+int proximity = 40;
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+
+
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1; // convert to cm
+  
+  if (distance < proximity) {  // This is where the LED On/Off happens
+    digitalWrite(led13,HIGH); // When the Red condition is met, the Green LED should turn off
+
+}
+  else {
+    digitalWrite(led13,LOW);
+
+  }
+  if (distance > 20){
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+ 
+}
